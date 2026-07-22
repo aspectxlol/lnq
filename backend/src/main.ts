66560b385f854590ac07b09ce9f4461f3def628b
Louie Hansen <gamernxt6@gmail.com>
2026-07-22 13:54:03 +0700

    bruh forgot to commit nest

---
+++ backend/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen({
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: process.env.HOST || '0.0.0.0',
+  });
+}
+
+bootstrap();
