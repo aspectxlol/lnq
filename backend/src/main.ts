f13185e8891364ba7ef131125dabb227ad43d601
Louie Hansen <gamernxt6@gmail.com>
2026-07-23 10:12:04 +0700

    feat: implement authentication module with controller, service, and DTOs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,23 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('LnQ Ecommerce API')
+    .setDescription('The LnQ Ecommerce API specification')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen({
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     host: process.env.HOST || '0.0.0.0',
